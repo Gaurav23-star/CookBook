a64d1f6c50fdf9d4fda4c37648e08d55cc1d8190
Jesus Hilario <jesus-hilario@outlook.com>
2023-10-09 18:09:37 -0400

    Made Basic Login Template

--- frontend/app/src/main/java/com/cookbook/MainActivity.java
+++ frontend/app/src/main/java/com/cookbook/MainActivity.java
@@ -2,14 +2,27 @@ package com.cookbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("Hellow World");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
