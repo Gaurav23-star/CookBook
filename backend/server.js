11321609d74a669e44414231a6b5e0898e459c55
Daniel Lusdyk <djl303@cs431-00.cs.rutgers.edu>
2023-10-10 19:57:44 -0400

    Define post method for user-defined-recipes endpoint

--- backend/server.js
+++ backend/server.js
@@ -17,33 +17,51 @@ const USER_DEFINED_RECIPES_ENDPOINT = '/user-defined-recipes';
 // Define tables
 const USER_DEFINED_RECIPES_TABLE = 'user_defined_recipes';
  
-// GET
+// GET method for user-defined-recipes
 app.get(USER_DEFINED_RECIPES_ENDPOINT, async (req, res) => {
     try {
         const result = await db.pool.query("select * from " + USER_DEFINED_RECIPES_TABLE);
-        res.send(result);
+        res.send(convertBigIntsToNumbers(result));
     } catch (err) {
-        throw err;
+        res.send(convertBigIntsToNumbers(err))
+        console.log(err);
     }
 });
  
-// POST
+// POST method for user-defined-recipes
 app.post(USER_DEFINED_RECIPES_ENDPOINT, async (req, res) => {
-    let userDefinedRecipe = req.body;
     try {
-        const result = await db.pool.query("insert into " + USER_DEFINED_RECIPES_TABLE + "(description) values (?)", [userDefinedRecipe.description]);
-        res.send(result);
+        const userDefinedRecipe = req.body;
+
+        const keys = [];
+        const values = [];
+        for (const [key, value] of Object.entries(userDefinedRecipe)) {
+            if (value !== null && value !== undefined) {
+                keys.push(key);
+                values.push(value);
+            }
+        }
+
+        const placeholders = values.map(() => "?").join(", ");
+        
+        const sql = `INSERT INTO ${USER_DEFINED_RECIPES_TABLE} (${keys.join(", ")}) VALUES (${placeholders})`;
+
+        const result = await db.pool.query(sql, values);
+
+        res.send(convertBigIntsToNumbers(result));
     } catch (err) {
-        throw err;
+        res.send(convertBigIntsToNumbers(err));
+        console.log(err);
     }
 });
- 
+
 app.put(USER_DEFINED_RECIPES_ENDPOINT, async (req, res) => {
     let task = req.body;
     try {
         const result = await db.pool.query("update tasks set description = ?, completed = ? where id = ?", [task.description, task.completed, task.id]);
         res.send(result);
     } catch (err) {
+        res.send(err);
         throw err;
     } 
 });
@@ -57,5 +75,17 @@ app.delete(USER_DEFINED_RECIPES_ENDPOINT, async (req, res) => {
         throw err;
     } 
 });
+
+function convertBigIntsToNumbers(obj) {
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            const value = obj[key];
+            if (typeof value === 'bigint') {
+                obj[key] = Number(value);
+            }
+        }
+    }
+    return obj;
+}
  
 app.listen(port, () => console.log(`Listening on port ${port}`));
