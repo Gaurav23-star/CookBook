5fcf39a368559eeba1337c90f0f00208032659e1
smthe <sam710@scarletmail.rutgers.edu>
2023-10-15 19:31:09 -0400

    getIsAdmin error

--- frontend/app/src/main/java/com/cookbook/HomeActivity.java
+++ frontend/app/src/main/java/com/cookbook/HomeActivity.java
@@ -4,21 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.cookbook.model.User;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HomeActivity extends AppCompatActivity implements RecyclerViewInterface{
 
     private static User currentUser;
-    boolean admin=false;
+    private static final String RECIPE_URL = "http://172.16.122.20:8080/user-defined-recipes";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +39,46 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewInter
 
         System.out.println("Current User " + currentUser.toString());
         setContentView(R.layout.activity_home);
+        final Thread thread = new Thread(() -> {
+            try {
+                final URL url = new URL(RECIPE_URL);
+                final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setDoOutput(true);
+                int userid = 1;
+                final String jsonData = "{\"user_id\":\"" + userid + "\"}";
+                System.out.println("Json Payload: " + jsonData);
+
+                final OutputStream os = connection.getOutputStream();
+                final OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+
+                osw.write(jsonData);
+                osw.flush();
+
+                final int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    final InputStream responseBody = connection.getInputStream();
+
+                    String jsonString = convertStreamToString(responseBody);
+                    System.out.println("Response body: " + jsonString);
+
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    JSONObject recipeJson = jsonObject.getJSONObject("recipe");
+
+                } else {
+
+                    throw new Exception("HTTP Request Failed with response code: " + responseCode);
+                }
+            } catch (Exception e) {
+
+                System.out.println("EXCEPTION OCcURRED " + e);
+
+            }
+        });
+
+        thread.start();
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Yummy apple pie!", "joemama258",R.drawable.applepie,"2hrs", "Apple", "Pie Crust"));
@@ -36,7 +86,7 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewInter
         items.add(new Item("Grandma's Tortilla Española", "smaye", R.drawable.tortilla, "45mins", "Eggs", "Potatoes"));
         items.add(new Item("One Pot Sausage and Vegs", "smaye", R.drawable.onepot, "30mins", "Sausage", "Potatoes"));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        MyAdapter adapter = new MyAdapter(getApplicationContext(),items,this);
+        MyAdapter adapter = new MyAdapter(getApplicationContext(),items,this, currentUser.getIsAdmin());
         //find way to hide admin
         recyclerView.setAdapter(adapter);
 
@@ -44,7 +94,7 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewInter
 
     @Override
     public void onItemClick(int position) {
-        if (!admin) {
+        if (currentUser.getIsAdmin()==0) {
             Toast.makeText(HomeActivity.this, "user this should open a page eventually ", Toast.LENGTH_SHORT).show();
         } else {
             final Dialog dialog = new Dialog(this);
@@ -57,4 +107,9 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewInter
             dialog.show();
         }
     }
+    private String convertStreamToString(InputStream is) {
+        final Scanner scanner = new Scanner(is, "UTF-8").useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
+    }
+
 }
