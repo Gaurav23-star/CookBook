01baf132d6182b492168bfd2d8ce8c682c88fbde
Jesus Hilario <jesus-hilario@outlook.com>
2023-10-18 14:45:45 -0400

    fixed user null error when switching between activities via navigation

--- frontend/app/src/main/java/com/cookbook/ProfileActivity.java
+++ frontend/app/src/main/java/com/cookbook/ProfileActivity.java
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.cookbook.model.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().hide();
+        //retrieve user passed in
+        currentUser = (User) getIntent().getSerializableExtra("current_user");
+
         handleNavigationChange();
     }
 
@@ -26,14 +32,25 @@ public class ProfileActivity extends AppCompatActivity {
                 case R.id.bottom_person:
                     return true;
                 case R.id.bottom_settings:
-                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    Intent intent_Settings = new Intent(getApplicationContext(), SettingsActivity.class);
+                    intent_Settings.putExtra("current_user",currentUser);
+                    startActivity(intent_Settings);
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                     finish();
+
+//                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+//                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+//                    finish();
                     return true;
                 case R.id.bottom_home:
-                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                    finish();
+                      Intent intent_Home = new Intent(getApplicationContext(), HomeActivity.class);
+                      intent_Home.putExtra("current_user",currentUser);
+                      startActivity(intent_Home);
+                      overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                      finish();
+//                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+//                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+//                    finish();
                     return true;
             }
             return false;
--- frontend/app/src/main/java/com/cookbook/SettingsActivity.java
+++ frontend/app/src/main/java/com/cookbook/SettingsActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.cookbook.model.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().hide();
+
+        //retrieve user passed in
+        currentUser = (User) getIntent().getSerializableExtra("current_user");
 
         handleNavigationChange();
     }
@@ -27,14 +32,26 @@ public class SettingsActivity extends AppCompatActivity {
                 case R.id.bottom_settings:
                     return true;
                 case R.id.bottom_person:
-                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    Intent intent_Person = new Intent(getApplicationContext(), ProfileActivity.class);
+                    intent_Person.putExtra("current_user",currentUser);
+                    startActivity(intent_Person);
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                     finish();
+//                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+//                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+//                    finish();
                     return true;
                 case R.id.bottom_home:
-                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                    Intent intent_Home = new Intent(getApplicationContext(), HomeActivity.class);
+                    intent_Home.putExtra("current_user",currentUser);
+                    startActivity(intent_Home);
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
                     finish();
+
+//                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+//                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+//                    finish();
                     return true;
             }
             return false;
